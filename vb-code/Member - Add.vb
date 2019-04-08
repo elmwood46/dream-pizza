4e04c8b89c4eb9868dbe426344b022bf5903547c
elmwood <49234279+elmwood46@users.noreply.github.com>
2019-04-09 09:03:43 +1200

    Add files via upload

---
+++ vb-code/Member - Add.vb
@@ -0,0 +1,257 @@
+﻿Public Class frm_addMember
+
+    '=====================================
+    ' FORM LOAD
+    '=====================================
+    Private Sub frm_addMember_ImeModeChanged(sender As Object, e As EventArgs) Handles Me.ImeModeChanged
+        'start timer
+        tim_blinktext.Enabled = False
+    End Sub
+
+    '=====================================
+    ' CANCEL BUTTON
+    '=====================================
+    Private Sub btn_cancel_Click(sender As Object, e As EventArgs) Handles btn_cancel.Click
+        Dim i As Integer = 0
+
+        'check to see if they started creating a member
+        For Each cntrl As Control In Me.Controls
+            If TypeOf cntrl Is TextBox Then
+                If Not cntrl.Text.Replace(" ", "") = "" Then
+                    i = MsgBox("Cancel member creation?", MsgBoxStyle.YesNo)
+                    Exit For
+                End If
+            End If
+        Next
+
+        If i = vbYes Or i = 0 Then
+            Me.Hide()
+            Me.Controls.Clear()
+            Me.InitializeComponent()
+            frm_members.Show()
+        End If
+    End Sub
+
+    '=====================================
+    ' CONFIRM BUTTON
+    '=====================================
+    Private Sub btn_confim_Click(sender As Object, e As EventArgs) Handles btn_confim.Click
+
+        ' check user input for speech marks
+        If checkSpeechMarks() = False Then
+            Exit Sub
+        End If
+
+        'CHECK FOR ALL INPUT
+        For Each cntrl As Control In Me.Controls
+            If TypeOf cntrl Is TextBox Or TypeOf cntrl Is ComboBox Then
+                If cntrl.Text.Replace(" ", "") = "" Then
+                    ' exit the for loop on no value
+                    If lbl_nullMsg.Visible = True Then
+                        ' start flashing timer
+                        tim_blinktext.Enabled = True
+                    Else
+                        lbl_nullMsg.Visible = True
+                    End If
+                    Exit Sub
+                End If
+            End If
+        Next
+
+        'IF ALL INPUTS ARE CONFIRMED:---------------------------
+        ' check for correct fields
+        If Not IsNumeric(txt_phone.Text.Replace(" ", "")) Then
+            MsgBox("Please enter a valid phone number.")
+            Exit Sub
+        End If
+
+        ' establish database variables
+        Dim con As New OleDb.OleDbConnection
+        Dim da As OleDb.OleDbDataAdapter
+        Dim ds As DataSet = New DataSet
+        Dim sql As String
+
+        'Open connection
+
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|\dreamPizza.accdb; Jet OLEDB:Database Password = ;"
+        con.Open()
+
+        'fill dataset with all data
+        sql = "SELECT * FROM tbl_members"
+
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.Fill(ds, "Anything")
+
+        ' check for maximum members
+        If ds.Tables(0).Columns(0).ExtendedProperties.Count >= 99998 Then
+            MsgBox("ERROR: Data capacity reached. Cannot add more members.")
+            Exit Sub
+        End If
+
+        ' define loop vars
+        Dim intRowCount, intColumnCount As Integer
+        intRowCount = ds.Tables(0).Rows.Count - 1
+        intColumnCount = ds.Tables(0).Columns.Count - 1
+
+        ' define checking vars
+        Dim fnameCheck = False
+        Dim lnameCheck = False
+        Dim phCheck = False
+
+        ' check input with existing database
+        For row = 0 To intRowCount
+            For col = 0 To intColumnCount
+                If ds.Tables(0).Rows(row).ItemArray(col).ToString.ToLower.Replace(" ", "") = txt_firstName.Text.ToLower.Replace(" ", "") Then
+                    fnameCheck = True
+                End If
+                If ds.Tables(0).Rows(row).ItemArray(col).ToString.ToLower.Replace(" ", "") = txt_lastName.Text.ToLower.Replace(" ", "") Then
+                    lnameCheck = True
+                End If
+                If ds.Tables(0).Rows(row).ItemArray(col) = txt_phone.Text.Replace(" ", "") Then
+                    phCheck = True
+                End If
+            Next col
+        Next row
+
+        ' display confirmation message if member exists
+        Dim i As Integer
+        If phCheck = True Then
+            ' check for phone and name
+            If fnameCheck = True AndAlso lnameCheck = True Then
+                i = MsgBox("A member with this name and phone number already exists. Continue anyway?", MsgBoxStyle.YesNo)
+                ' check for just phone
+            Else
+                i = MsgBox("A member with this phone number already exists. Continue anyway?", MsgBoxStyle.YesNo)
+            End If
+        Else
+            ' check for no phone but name
+            If fnameCheck = True AndAlso lnameCheck = True Then
+                i = MsgBox("A member with this name already exists. Continue anyway?", MsgBoxStyle.YesNo)
+                ' no data match
+            Else
+                i = vbYes
+            End If
+        End If
+
+        ' CONFIRMED MEMBER ADDED
+        If i = vbYes Then
+
+            ' GENERATE UNIQUE MEMBER ID
+            Dim randomClass As New Random()
+            Dim memval As New Integer
+            memval = randomClass.Next(1, 99999)
+
+            For row = 0 To intRowCount
+                If memval = ds.Tables(0).Rows(row).ItemArray(0) Then
+                    Do While memval = ds.Tables(0).Rows(row).ItemArray(0)
+                        memval = randomClass.Next(1, 99999)
+                    Loop
+                    row = 0
+                End If
+            Next row
+
+            'set sqlarray(0)
+            Dim sqlArray(7)
+            sqlArray(0) = memval.ToString
+            sqlArray(1) = cbox_title.Text
+            sqlArray(2) = txt_firstName.Text
+            sqlArray(3) = txt_lastName.Text
+            sqlArray(4) = txt_address.Text
+            sqlArray(5) = txt_phone.Text.Replace(" ", "")
+            sqlArray(6) = cbox_member.Text
+            sqlArray(7) = txt_delivery.Text
+
+            ' repair text
+            For n = 1 To 7
+                sqlArray(n).Replace("'", """")
+            Next
+
+            ' sql insert
+            Dim sqlInsertQuery As New OleDb.OleDbCommand
+            sql = "INSERT INTO tbl_members (memberNo, title, firstName, lastName, address, phoneNo, membership, delivery) VALUES ("
+
+            ' add to the sql 
+            For n = 0 To 7
+                sql = sql & "'" & sqlArray(n) & "'"
+                If Not n = 7 Then
+                    sql = sql + ","
+                Else
+                    sql = sql + ");"
+                End If
+            Next
+
+            ' insert into database & close connection
+            ' check state
+            Console.Write("State is " & con.State)
+
+            sqlInsertQuery.Connection = con
+            sqlInsertQuery.CommandText = sql
+            sqlInsertQuery.ExecuteNonQuery()
+            sqlInsertQuery.Dispose()
+            da.Dispose()
+            ds.Dispose()
+            con.Close()
+            con.Dispose()
+            memval = 0
+            sql = ""
+
+            ' clear fields & wrap up
+            MsgBox("Member Added")
+            For Each cntrl As Control In Me.Controls
+                If TypeOf cntrl Is TextBox Or TypeOf cntrl Is ComboBox Then
+                    cntrl.ResetText()
+                End If
+            Next
+
+            ' exit form
+            Me.Hide()
+            Me.Controls.Clear()
+            Me.InitializeComponent()
+            frm_members.Show()
+
+        Else
+            ' if they don't want to add a member with same name/phone, end
+            da.Dispose()
+            ds.Dispose()
+            con.Close()
+            con.Dispose()
+        End If
+    End Sub
+
+    '=====================================
+    ' TIMER TICK
+    '=====================================
+    Dim timer As Integer = 0
+    Private Sub tim_blinktext_Tick(sender As Object, e As EventArgs) Handles tim_blinktext.Tick
+        'toggle timer switch
+        timer += 1
+
+        If lbl_nullMsg.Visible = False Then
+            lbl_nullMsg.Visible = True
+        Else
+            lbl_nullMsg.Visible = False
+        End If
+
+        If timer = 10 Then
+            tim_blinktext.Stop()
+            timer = 0
+        End If
+    End Sub
+
+    '=====================================
+    ' DELIVERY TEXT CHANGED
+    '=====================================
+    Private Sub txt_delivery_TextChanged(sender As Object, e As EventArgs) Handles txt_delivery.TextChanged
+        lab_deliveryText.Text = txt_delivery.TextLength & "/" & txt_delivery.MaxLength
+    End Sub
+
+    '=====================================
+    ' VISIBLE CHANGED
+    '=====================================
+    Private Sub frm_addMember_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
+        If Me.Visible Then
+            ' if I am visible, set the initial value of the character count label
+            lab_deliveryText.Text = "0/" & txt_delivery.MaxLength
+        End If
+    End Sub
+End Class
---
+++ vb-code/Member - Edit.vb
@@ -0,0 +1,260 @@
+﻿Public Class frm_editMember
+
+    ' create initial saving values
+    Dim orderedControls(6)
+    Dim initialValue(6)
+    Dim selectedMember = currentSelected
+
+    '=====================================
+    ' FORM VISIBLE
+    '=====================================
+    Private Sub frm_editMember_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
+        If Me.Visible = True Then
+            ' set the edited member
+            selectedMember = currentSelected
+            currentSelected = ""
+            ' populate form with data
+            'establish database variables
+            Dim con As New OleDb.OleDbConnection
+            Dim da As OleDb.OleDbDataAdapter
+            Dim ds As DataSet = New DataSet
+            Dim sql As String
+
+            'Open connection
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|\dreamPizza.accdb; Jet OLEDB:Database Password = ;"
+            con.Open()
+
+            'fill dataset with all data
+            sql = "SELECT [title], [firstName], [lastName], [address], [phoneNo], [membership], [delivery] FROM [tbl_members] WHERE memberNo = " & selectedMember
+
+            da = New OleDb.OleDbDataAdapter(sql, con)
+            da.Fill(ds, "Anything")
+
+            ' record the controls in order
+            orderedControls(0) = cbox_title_edit
+            orderedControls(1) = txt_firstName_edit
+            orderedControls(2) = txt_lastName_edit
+            orderedControls(3) = txt_address_edit
+            orderedControls(4) = txt_phone_edit
+            orderedControls(5) = cbox_member_edit
+            orderedControls(6) = txt_delivery_edit
+
+            ' fill template with data & record the initial values of the data
+            lab_memberName.Text = selectedMember
+            For n = 0 To 6
+                orderedControls(n).text = ds.Tables(0).Rows(0).ItemArray(n)
+                initialValue(n) = ds.Tables(0).Rows(0).ItemArray(n)
+            Next
+            con.Close()
+
+        End If
+    End Sub
+
+    '=====================================
+    ' CONFIRM BUTTON
+    '=====================================
+    Private Sub btn_confirm_Click(sender As Object, e As EventArgs) Handles btn_confirm.Click
+
+        ' check user input for speech marks
+        If checkSpeechMarks() = False Then
+            Exit Sub
+        End If
+
+        'CHECK FOR BOXES FILLED
+        For n = 0 To 6
+            If orderedControls(n).text = "" Then
+                ' exit the for loop on no value
+                If lab_nullMsg.Visible = True Then
+                    ' start flashing timer
+                    tim_blinktext.Enabled = True
+                Else
+                    lab_nullMsg.Visible = True
+                End If
+                Exit Sub
+            End If
+        Next
+
+        'IF ALL INPUTS ARE CONFIRMED:---------------------------
+        ' CHECK FOR VALID PHONE
+        If Not IsNumeric(txt_phone_edit.Text.Replace(" ", "")) Then
+            MsgBox("Please enter a valid phone number.")
+            Exit Sub
+        End If
+        ' -------------------------------------------------------
+
+        'UPDATE DATABASE: 
+        ' establish database variables
+        Dim con As New OleDb.OleDbConnection
+        Dim da As OleDb.OleDbDataAdapter
+        Dim ds As DataSet = New DataSet
+        Dim sql As String = ""
+
+        'Open connection
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|dreamPizza.accdb; Jet OLEDB:Database Password = ;"
+        con.Open()
+
+        'fill dataset with all data
+        Dim short_table(6)
+        short_table(0) = "[title]"
+        short_table(1) = "[firstName]"
+        short_table(2) = "[lastName]"
+        short_table(3) = "[address]"
+        short_table(4) = "[phoneNo]"
+        short_table(5) = "[membership]"
+        short_table(6) = "[delivery]"
+
+        Dim sqlInsertQuery As New OleDb.OleDbCommand(sql, con)
+
+        sql = "UPDATE tbl_members SET "
+        For n = 0 To 6
+            sql = sql & short_table(n) & " = " & "'" & orderedControls(n).text & "'"
+            If Not n = 6 Then
+                sql = sql & ", "
+            End If
+        Next
+        sql = sql & " WHERE memberNo = " & selectedMember
+
+        ' update database and close connection
+        sqlInsertQuery.Connection = con
+        sqlInsertQuery.CommandText = sql
+        sqlInsertQuery.ExecuteNonQuery()
+        sqlInsertQuery.Dispose()
+        ds.Dispose()
+        con.Close()
+        con.Dispose()
+
+        MsgBox("Member #" & selectedMember & " was updated successfully.")
+        exit_form()
+    End Sub
+
+    '=====================================
+    ' EXIT FORM SUB
+    '=====================================
+    Sub exit_form()
+        selectedMember = ""
+        Me.Hide()
+        'CenterToScreen()
+        frm_members.Show()
+    End Sub
+
+    '=====================================
+    ' EXIT BUTTON
+    '=====================================
+    Private Sub btn_exit_Click(sender As Object, e As EventArgs) Handles btn_exit.Click
+        Dim i = vbNo
+        For n = 0 To 6
+            If Not orderedControls(n).text = initialValue(n) Then
+                i = MsgBox("Changes have been made. Exit anyway?", MsgBoxStyle.YesNo)
+                Exit For
+            ElseIf n = 6 Then
+                i = vbYes
+            End If
+        Next
+        If i = vbYes Then
+            exit_form()
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+    '=====================================
+    ' RESET BUTTON
+    '=====================================
+    Private Sub btn_reset_Click(sender As Object, e As EventArgs) Handles btn_reset.Click
+        Dim i = MsgBox("Reset values?", MsgBoxStyle.YesNo)
+        If i = vbYes Then
+            For n = 0 To 6
+                orderedControls(n).text = initialValue(n)
+            Next
+        End If
+    End Sub
+
+    '=====================================
+    ' LABEL TIMER TICK
+    '=====================================
+    Dim timer As Integer = 0
+    Private Sub tim_blinktext_Tick(sender As Object, e As EventArgs) Handles tim_blinktext.Tick
+        'toggle timer switch
+        Timer += 1
+
+        If lab_nullMsg.Visible = False Then
+            lab_nullMsg.Visible = True
+        Else
+            lab_nullMsg.Visible = False
+        End If
+
+        If Timer = 10 Then
+            tim_blinktext.Stop()
+            Timer = 0
+        End If
+    End Sub
+
+    '=====================================
+    ' DELETE MEMBER
+    '=====================================
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        Dim r = MsgBox("Are you sure you want to delete this member?", MsgBoxStyle.YesNo)
+        If r = MsgBoxResult.Yes Then
+
+            ' establish database variables
+            Dim con As New OleDb.OleDbConnection
+            'Dim da As OleDb.OleDbDataAdapter
+            Dim ds As DataSet = New DataSet
+            Dim sql As String = ""
+
+            'Open connection
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|dreamPizza.accdb; Jet OLEDB:Database Password = ;"
+            con.Open()
+            sql = "DELETE * FROM tbl_members WHERE memberNo = " & selectedMember
+            Dim sqlDeleteQuery As New OleDb.OleDbCommand(sql, con)
+
+            ' delete from database and close connection
+            sqlDeleteQuery.Connection = con
+            sqlDeleteQuery.CommandText = sql
+            sqlDeleteQuery.ExecuteNonQuery()
+            sqlDeleteQuery.Dispose()
+            ds.Dispose()
+            con.Close()
+            con.Dispose()
+
+            'exit form
+            MsgBox("Member #" & selectedMember & " was deleted.")
+            exit_form()
+        End If
+    End Sub
+
+    '=====================================
+    ' ENABLE BUTTONS TIMER
+    '=====================================
+    Private Sub tim_checkControls_Tick(sender As Object, e As EventArgs) Handles tim_checkControls.Tick
+        ' CHECK FOR NO CHANGES AT ALL
+        Dim changed As Boolean = False
+        ' turn buttons off if no edits were made
+        For n = 0 To 6
+            If Not orderedControls(n).text = initialValue(n) Then
+                changed = True
+            End If
+        Next
+        If changed = False Then
+            btn_confirm.Enabled = False
+            btn_reset.Enabled = False
+        Else
+            btn_confirm.Enabled = True
+            btn_reset.Enabled = True
+        End If
+    End Sub
+
+    '=====================================
+    ' CHANGE "delivery" textbox text
+    '=====================================
+    Private Sub txt_delivery_TextChanged(sender As Object, e As EventArgs) Handles txt_delivery_edit.TextChanged
+        lab_directionNumber.Text = txt_delivery_edit.TextLength & "/" & txt_delivery_edit.MaxLength
+    End Sub
+
+    '=====================================
+    ' Change "address" textbox text
+    '=====================================
+    Private Sub txt_address_TextChanged(sender As Object, e As EventArgs) Handles txt_address_edit.TextChanged
+        lab_addressNumber.Text = txt_address_edit.TextLength & "/" & txt_address_edit.MaxLength
+    End Sub
+End Class
